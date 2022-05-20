365280a279366256aa2df1611a918d6afb98e00f
Nabeel Hassan <60441195+nabeelbaghoor@users.noreply.github.com>
2022-05-20 11:18:15 +0500

    Added wakelock

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:kiosk_mode/kiosk_mode.dart';
+import 'package:wakelock/wakelock.dart';
 
 void main() {
   runApp(const App());
@@ -62,6 +63,48 @@ class _HomeState extends State<_Home> {
                 'Current mode: ${snapshot.data}',
               ),
             ),
+          Divider(),
+          OutlinedButton(
+            onPressed: () {
+              // The following code will enable the wakelock on the device
+              // using the wakelock plugin.
+              setState(() {
+                Wakelock.enable();
+                // You could also use Wakelock.toggle(on: true);
+              });
+            },
+            child: const Text('enable wakelock'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              // The following code will disable the wakelock on the device
+              // using the wakelock plugin.
+              setState(() {
+                Wakelock.disable();
+                // You could also use Wakelock.toggle(on: false);
+              });
+            },
+            child: const Text('disable wakelock'),
+          ),
+          const Spacer(
+            flex: 2,
+          ),
+          FutureBuilder(
+            future: Wakelock.enabled,
+            builder: (context, AsyncSnapshot<bool> snapshot) {
+              final data = snapshot.data;
+              // The use of FutureBuilder is necessary here to await the
+              // bool value from the `enabled` getter.
+              if (data == null) {
+                // The Future is retrieved so fast that you will not be able
+                // to see any loading indicator.
+                return Container();
+              }
+
+              return Text('The wakelock is currently '
+                  '${data ? 'enabled' : 'disabled'}.');
+            },
+          ),
         ],
       );
 }
